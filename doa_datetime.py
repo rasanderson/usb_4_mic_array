e8cfe60590ce2b1e106a186ab8fef7d3764eadb0
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2024-03-17 12:52:52 +0000

    Correctly prints from two microphones

--- doa_datetime.py
+++ doa_datetime.py
@@ -4,9 +4,13 @@ import usb.util
 import datetime
 import time
 
-dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
-devices = usb.core.find(find_all=True, idVendor=0x2886, idProduct=0x0018)
-print(devices)
+# ReSpeaker USB Mic Array IDs
+VENDOR_ID = 0x2886
+PRODUCT_ID = 0x0018
+
+# Find all devices
+#dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+devices = list(usb.core.find(find_all=True, idVendor=VENDOR_ID, idProduct=PRODUCT_ID))
 
 for device in devices:
     print(f"Microphone found: Bus {device.bus} Device {device.address}")
@@ -14,16 +18,18 @@ for device in devices:
 
 start_time = time.time()
 
-if dev:
-    Mic_tuning = Tuning(dev)
+print("DateTime, ElapsedTime, DOA1, DOA2")
+if devices:
+    Mic1_tuning = Tuning(devices[0])
+    Mic2_tuning = Tuning(devices[1])
     elapsed_seconds = time.time() - start_time
     now = datetime.datetime.now()
-    print(now,", ",f"{elapsed_seconds:.2f}", ", ", Mic_tuning.direction)
+    print(now,", ",f"{elapsed_seconds:.2f}", ", ", Mic1_tuning.direction, ", ", Mic2_tuning.direction)
     while True:
         try:
             elapsed_seconds = time.time() - start_time
             now = datetime.datetime.now()
-            print(now,", ",f"{elapsed_seconds:.2f}", ", ", Mic_tuning.direction)
+            print(now,", ",f"{elapsed_seconds:.2f}", ", ", Mic1_tuning.direction, ", ", Mic2_tuning.direction)
             time.sleep(1.5)
         except KeyboardInterrupt:
             break
