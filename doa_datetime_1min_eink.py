15452a82ddd80e41474b794f93b1e99b5f932b65
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2025-03-24 16:34:40 +0000

    Added doa_datetime_1min_eink.py which displays angles on screen

---
+++ doa_datetime_1min_eink.py
@@ -0,0 +1,89 @@
+from tuning import Tuning
+import usb.core
+import usb.util
+import datetime
+import time
+
+# ReSpeaker USB Mic Array IDs
+VENDOR_ID = 0x2886
+PRODUCT_ID = 0x0018
+MAX_SECONDS = 60
+
+# eink setup stuff
+import sys
+import os
+current_dir = os.path.dirname(os.path.realpath(__file__))
+libdir = os.path.join(current_dir, 'eink_files/lib')
+picdir = os.path.join(current_dir, 'eink_files/pic')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
+from waveshare_epd import epd3in52
+import time
+from PIL import Image,ImageDraw,ImageFont
+import traceback
+from pijuice import PiJuice
+pijuice = PiJuice()
+
+epd = epd3in52.EPD()
+epd.init()
+epd.display_NUM(epd.WHITE)
+epd.lut_GC()
+epd.refresh()
+font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+font30 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
+
+output_file = "/home/nras/output_doa.csv"
+
+# Find all devices
+#dev = usb.core.find(idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
+devices = list(usb.core.find(find_all=True, idVendor=VENDOR_ID, idProduct=PRODUCT_ID))
+
+#for device in devices:
+#    print(f"Microphone found: Bus {device.bus} Device {device.address}")
+
+
+with open('/sys/bus/usb/devices/1-1.3/devnum', 'r') as file:
+    usb_bl = int(file.read().strip())
+with open('/sys/bus/usb/devices/1-1.5/devnum', 'r') as file:
+    usb_br = int(file.read().strip())
+with open('/sys/bus/usb/devices/1-1.2/devnum', 'r') as file:
+    usb_tl = int(file.read().strip())
+with open('/sys/bus/usb/devices/1-1.4/devnum', 'r') as file:
+    usb_tr = int(file.read().strip())
+
+devices[0].address = usb_bl
+devices[1].address = usb_br
+devices[2].address = usb_tl
+devices[3].address = usb_tr
+
+start_time = time.time()
+
+with open(output_file, "a") as outfile:
+    print("DateTime, ElapsedTime, DOA1bl, DOA2br, DOA3tl, DOA4tr", file=outfile)
+    if devices:
+        Mic1_tuning = Tuning(devices[0])
+        Mic2_tuning = Tuning(devices[1])
+        Mic3_tuning = Tuning(devices[2])
+        Mic4_tuning = Tuning(devices[3])
+        elapsed_seconds = time.time() - start_time
+        now = datetime.datetime.now()
+        print(now,", ",f"{elapsed_seconds:.2f}", ", ", Mic1_tuning.direction, ", ", Mic2_tuning.direction, ", ", Mic3_tuning.direction,", ", Mic4_tuning.direction, file=outfile)
+        while elapsed_seconds <= MAX_SECONDS:
+
+            try:
+                elapsed_seconds = time.time() - start_time
+                now = datetime.datetime.now()
+                print(now,", ",f"{elapsed_seconds:.2f}", ", ", Mic1_tuning.direction, ", ", Mic2_tuning.direction, ", ", Mic3_tuning.direction,", ", Mic4_tuning.direction, file=outfile)
+                time.sleep(0.5)
+                if int(elapsed_seconds) % 5 == 0:
+                    # Create info for eink display
+                    image = Image.new('1', (epd.height, epd.width), 255)
+                    draw = ImageDraw.Draw(image)
+                    draw.text((10,20), f"Recording now {elapsed_seconds:.2f}", font = font24, fill=0)
+                    draw.text((10,50), f"Mic1: {Mic1_tuning.direction:.0f} Mic2: {Mic2_tuning.direction:.0f} Mic3: {Mic3_tuning.direction:.0f} Mic4: {Mic4_tuning.direction:.0f}", font = font18, fill=0)
+                    epd.display(epd.getbuffer(image))
+                    epd.refresh()
+            except KeyboardInterrupt:
+                break
